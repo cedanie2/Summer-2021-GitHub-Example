22efe1c90f3c8a5a31ffeb328c7b3b500b7dec67
Clinton Daniel <cedanie2@usf.edu>
2021-05-27 21:00:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summer_2021_GitHub_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
